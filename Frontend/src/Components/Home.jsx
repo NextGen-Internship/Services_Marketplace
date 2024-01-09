ac3af1a6b51257727b1b04d1d49d4a3a0ae1d586
Borislav Nikolov <borislav.nikolov@blankfactor.com>
2024-01-09 10:28:05 +0200

    added folders structure

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -1,30 +1,27 @@
 // Home.js
 import React from 'react';
-import '../styles/Home.css'
-import { FaSearch } from "react-icons/fa";
+import '../styles/Home.css';
+import { FaSearch } from 'react-icons/fa';
 
 export const Home = () => {
   return (
     <div className='home-wrapper'>
-      <div className='home-page'>
-        <div className='search-box'>
-          <form action=''>
-            <div>
-              <input
-                className='input-search'
-                type='text'
-                placeholder='Search here...'
-                autoComplete='off'
-                required
-              />
-            
-              <button className='search-btn'><FaSearch className='icon-input' /></button>
-              
-            </div>
-          </form>
+      <div className='search-box'>
+        <div className='search-container'>
+          <input
+            className='input-search'
+            type='text'
+            placeholder='Search here...'
+            autoComplete='off'
+            required
+          />
+          <button className='search-btn'>
+            <FaSearch className='icon-input' />
+          </button>
         </div>
       </div>
-    </div>
+
+      </div>
   );
 };
 
