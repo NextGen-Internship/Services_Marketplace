66dadadc7dd11256f774b6a9f53a96c7474139ea
Borislav Nikolov <borislav.nikolov@blankfactor.com>
2024-01-09 13:46:32 +0200

    Add configuration for jwt authorization.

---
+++ Backend/src/main/java/com/service/marketplace/controller/AuthenticationController.java
@@ -0,0 +1,34 @@
+package com.service.marketplace.controller;
+
+
+import com.service.marketplace.dto.request.AuthenticationRequest;
+import com.service.marketplace.dto.request.RegisterRequest;
+import com.service.marketplace.dto.response.AuthenticationResponse;
+import com.service.marketplace.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(service.login(request));
+    }
+}
---
+++ Backend/src/main/java/com/service/marketplace/service/AuthenticationService.java
@@ -0,0 +1,55 @@
+package com.service.marketplace.service;
+
+
+
+import com.service.marketplace.dto.request.AuthenticationRequest;
+import com.service.marketplace.dto.request.RegisterRequest;
+import com.service.marketplace.dto.response.AuthenticationResponse;
+import com.service.marketplace.user.Role;
+import com.service.marketplace.user.User;
+import com.service.marketplace.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.CUSTOMER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse login(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
