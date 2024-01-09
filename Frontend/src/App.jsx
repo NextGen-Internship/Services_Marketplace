1fdace348f697513d5ca5c76cb5caeaf48fb4acc
BNikolov13 <152274804+BNikolov13@users.noreply.github.com>
2024-01-09 14:20:09 +0200

    Login and registration form (#3)
    * commit login and registration form
    
    * icons deleted.
    
    * added folders structure
    
    * Delete username input property from signup page
    
    ---------
    
    Co-authored-by: vasilhristov1 <>

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,26 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import { useState } from 'react';
+import Menu from './Components/Menu';
+import Home from './Components/Home';
+import Category from './Components/Category';
+import { SignIn } from './Components/Access/SignIn';
+import SignUp from './Components/Access/SignUp';
+
 function App() {
+  const[clicked, isClicked] = useState(false)
   return (
-    <>
-      <h1>Service Marketplace Frontend</h1>
-    </>
+    <Router>
+      <Navbar clicked={clicked} isClicked={isClicked}/>
+      {clicked? <Menu/>:null}
+      <Routes>
+        <Route exact path="home-page" element={<Home/>}/>
+        <Route exact path="category-page" element={<Category/>}/>
+        <Route exact path="sign-in" element={<SignIn/>}/>
+        <Route exact path="sign-up" element={<SignUp/>}/>
+      </Routes>
+    </Router>
   );
 }
 
