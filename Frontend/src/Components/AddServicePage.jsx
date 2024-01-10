a9c918e80d9c211c5fefb025d579ea47ced0e2db
vasilhristov1 <>
2024-01-10 13:13:37 +0200

    Modified the AddServicePage.

--- Frontend/src/Components/AddServicePage.jsx
+++ Frontend/src/Components/AddServicePage.jsx
@@ -1,30 +1,86 @@
 import React from 'react'
 import AddService from './AddService'
+import { useState, useEffect } from 'react';
 
 function AddServicePage() {
-    const onAdd = async (service) => {
-        console.log(service);
-        // const id = Math.floor(Math.random() * 10000) + 1;
-        // const newTask = { id, ...task };
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+        try {
+            const response = await fetch('http://localhost:8080/v1/services');
+            if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const data = await response.json();
+            setServices(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+        };
     
-    //     await fetch('http://localhost:8080/v1/task/create', {
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify(task),
-    //   })
-    //     .then((response) => response.json())
-    //     .then((data) => {
-    //       console.log(data); 
-    //       setTasks([...tasks, data]); 
-    //     })
-    //     .catch((error) => console.error(error));
-      }
+        fetchData();
+    }, []);
+    
+
+    // add service
+    const addService = async (service) => {
+        const id = Math.floor(Math.random() * 10000) + 1;
+        // const newTask = { id, ...task };
+
+        await fetch('http://localhost:8080/v1/services/create', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(service),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+        console.log(data); 
+        setServices([...services, data]); 
+        })
+        .catch((error) => console.error(error));
+    }
+
+    // delete service
+    const deleteService = async (id) => {
+        setServices(services.filter((service) => service.id !== id));
+
+        await fetch(`http://localhost:8080/v1/services/delete/${id}`, {
+        method: 'DELETE',
+        })
+        .then((response) => {
+        if (!response.ok) {
+            throw new Error(`Failed to delete task with id ${id}`);
+        }
+        })
+        .catch((error) => console.error(error));
+    };
+
+    // const onAdd = async (service) => {
+    //     console.log(service);
+    //     // const id = Math.floor(Math.random() * 10000) + 1;
+    //     // const newTask = { id, ...task };
+    
+    // //     await fetch('http://localhost:8080/v1/task/create', {
+    // //     method: 'POST',
+    // //     headers: {
+    // //       'Content-Type': 'application/json',
+    // //     },
+    // //     body: JSON.stringify(task),
+    // //   })
+    // //     .then((response) => response.json())
+    // //     .then((data) => {
+    // //       console.log(data); 
+    // //       setTasks([...tasks, data]); 
+    // //     })
+    // //     .catch((error) => console.error(error));
+    //   }
 
   return (
     <div>
-        <AddService onAdd={onAdd} />
+        <AddService onAdd={addService} />
     </div>
   )
 }
