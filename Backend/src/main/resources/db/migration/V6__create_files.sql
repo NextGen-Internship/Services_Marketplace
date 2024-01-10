491c81a2364c76b9719b4620c2a9c9c37ebd5a48
Borislav Nikolov <borislav.nikolov@blankfactor.com>
2024-01-10 10:44:47 +0200

    fix migration

--- Backend/src/main/resources/db/migration/V6__create_files.sql
+++ Backend/src/main/resources/db/migration/V6__create_files.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS files (
     id INT AUTO_INCREMENT,
-    review_id INT NOT NULL,
+    review_id INT ,
     url VARCHAR(255),
     expiration_date DATETIME,
     PRIMARY KEY (id),
