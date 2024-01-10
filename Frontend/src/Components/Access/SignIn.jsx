aa2c08bd1637046ca0d51a7f2dabd7081dac7e91
BNikolov13 <152274804+BNikolov13@users.noreply.github.com>
2024-01-10 15:19:02 +0200

    Google-login (#6)
    * commit google-login
    
    * fix google-login
    
    * added google key

--- Frontend/src/Components/Access/SignIn.jsx
+++ Frontend/src/Components/Access/SignIn.jsx
@@ -1,44 +1,62 @@
-// SignIn.js
 import React from 'react';
-import '../../styles/SignIn.css'
-import { FaUserTie } from 'react-icons/fa6';
+import '../../styles/SignIn.css';
+import { FaUserTie, FaFacebook } from 'react-icons/fa';
 import { IoIosLock } from 'react-icons/io';
-import { FaGoogle } from 'react-icons/fa';
-import { FaFacebook } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useGoogleLogin, GoogleLogin } from '@react-oauth/google';
 
 export const SignIn = () => {
+  const navigate = useNavigate();
+
+  const responseGoogle = (response) => {
+    console.log(response);
+    // Handle Google login response here if needed
+  };
+
   return (
     <div className='sign-in'>
-      <form action="">
-        <h1>Login</h1>
-        <div className='input-box'>
-          <input type='text' placeholder='Username' required />
-          <FaUserTie className='icon' />
+      <div>
+        <form action=''>
+          <h1>Login</h1>
+          <div className='input-box'>
+            <input type='text' placeholder='Username' required />
+            <FaUserTie className='icon' />
+          </div>
+          <div className='input-box'>
+            <input type='password' placeholder='Password' required />
+            <IoIosLock className='icon' />
+          </div>
+          <div className='remember-forgot'>
+            <label>
+              <input type='checkbox' />Remember me
+            </label>
+            <a href='#'>Forgot password</a>
+          </div>
+          <button type='submit'>Login</button>
+          <div className='register-link'>
+            <p>
+              Don't have an account? <Link to='/sign-up'>Register</Link>
+            </p>
+          </div>
+        </form>
+        <div className='media-options'>
+          <GoogleLogin 
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            clientId="350761079008-0ipa8rk7sumieir1rq4b5ljg3pu78trt.apps.googleusercontent.com"
+            buttonText="Login with Google"
+            cookiePolicy={'single_host_origin'}
+          />
+          <button onClick={() => console.log('login with FB click')}>
+            <div className='media-options-login-buttons'>
+              <FaFacebook className='icon' />
+              <span>Login with Facebook</span>
+            </div>
+          </button>
         </div>
-        <div className='input-box'>
-          <input type='password' placeholder='Password' required />
-          <IoIosLock className='icon' />
-        </div>
-        <div className='remember-forgot'>
-          <label><input type='checkbox' />Remember me</label>
-          <a href='#'>Forgot password</a>
-        </div>
-        <button type='submit'>Login</button>
-        <div className='register-link'>
-          <p>Don't have an account? <Link to='/sign-up'>Register</Link></p>
-        </div>
-      </form>
-      <div className='media-options'>
-        <a href='#'>
-          <FaGoogle className='icon' />
-          <span>Login with Google</span>
-        </a>
-        <a href='#'>
-          <FaFacebook className='icon' />
-          <span>Login with Facebook</span>
-        </a>
       </div>
     </div>
   );
 };
+
+export default SignIn;
