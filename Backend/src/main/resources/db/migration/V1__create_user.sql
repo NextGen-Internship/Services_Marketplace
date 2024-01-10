0f805c4af4fb4a9594decb8cbf0ceb21274fbba0
vasilhristov1 <>
2024-01-10 08:38:43 +0200

    Migrations modified.

--- Backend/src/main/resources/db/migration/V1__create_user.sql
+++ Backend/src/main/resources/db/migration/V1__create_user.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS user (
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    phone_number INT UNIQUE NOT NULL,
+    phone_number VARCHAR(50) UNIQUE NOT NULL,
     experience INT,
     rating DOUBLE,
     description VARCHAR(255),
