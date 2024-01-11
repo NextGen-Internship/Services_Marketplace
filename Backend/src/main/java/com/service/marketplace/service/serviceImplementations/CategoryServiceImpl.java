384c1ad8a1155fabd79e361352fbf1964baadcd6
vasilhristov1 <>
2024-01-11 15:31:51 +0200

    Commented code deleted.

--- Backend/src/main/java/com/service/marketplace/service/serviceImplementations/CategoryServiceImpl.java
+++ Backend/src/main/java/com/service/marketplace/service/serviceImplementations/CategoryServiceImpl.java
@@ -1,20 +1,18 @@
 package com.service.marketplace.service.serviceImplementations;
 
 import com.service.marketplace.dto.request.CategoryRequest;
-import com.service.marketplace.dto.response.CategoryResponse;
 import com.service.marketplace.mapper.CategoryMapper;
 import com.service.marketplace.persistence.entity.Category;
 import com.service.marketplace.persistence.repository.CategoryRepository;
 import com.service.marketplace.service.CategoryService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @AllArgsConstructor
-public class CategoryServiceImplementation implements CategoryService {
+public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
     private final CategoryMapper categoryMapper;
 
@@ -41,8 +39,6 @@ public class CategoryServiceImplementation implements CategoryService {
         Category existingCategory = categoryRepository.findById(categoryId).orElse(null);
 
         if (existingCategory != null) {
-//            existingCategory.setName(categoryToUpdate.getName());
-//            existingCategory.setDescription(categoryToUpdate.getDescription());
             existingCategory = categoryMapper.categoryRequestToCategory(categoryToUpdate);
 
             return categoryRepository.save(existingCategory);
