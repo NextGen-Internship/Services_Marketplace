f8b4d5a254c9d79cb88552a7cedc76395f52dd8d
vasilhristov1 <>
2024-01-15 13:02:54 +0200

    Services page added.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,6 +9,7 @@ import { SignIn } from './Components/Access/SignIn';
 import SignUp from './Components/Access/SignUp';
 import AddServicePage from './Components/AddServicePage';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import ServicesPage from './Components/ServicesPage';
 
 function App() {
   const[clicked, isClicked] = useState(false)
@@ -21,6 +22,7 @@ function App() {
           <Route exact path="home-page" element={<Home/>}/>
           <Route exact path="category-page" element={<Category/>}/>
           <Route exact path="add-service-page" element={<AddServicePage/>}/>
+          <Route exact path="services" element={<ServicesPage/>}/>
           <Route exact path="sign-in" element={<SignIn/>}/>
           <Route exact path="sign-up" element={<SignUp/>}/>
           
