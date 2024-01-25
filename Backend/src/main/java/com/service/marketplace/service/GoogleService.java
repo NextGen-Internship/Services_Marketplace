fe5a77e21c72fd45da4e651edd527da0e8445ab4
Borislav Nikolov <borislav.nikolov@blankfactor.com>
2024-01-25 16:11:47 +0200

    set role

--- Backend/src/main/java/com/service/marketplace/service/GoogleService.java
+++ Backend/src/main/java/com/service/marketplace/service/GoogleService.java
@@ -5,13 +5,17 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.service.marketplace.dto.response.AuthenticationResponse;
+import com.service.marketplace.persistence.entity.Role;
 import com.service.marketplace.persistence.entity.User;
+import com.service.marketplace.persistence.repository.RoleRepository;
 import com.service.marketplace.persistence.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Service
@@ -20,6 +24,7 @@ public class GoogleService {
 
     private final UserRepository userRepository;
     private final JwtService jwtService;
+    private RoleRepository roleRepository;
 
 
     public AuthenticationResponse verifyGoogleToken(String googleToken) throws IOException, GeneralSecurityException {
@@ -39,6 +44,9 @@ public class GoogleService {
                 return new AuthenticationResponse(jwtToken);
 
             } catch (Exception e) {
+                Role role = roleRepository.findByName("CUSTOMER").orElseThrow();
+                Set<Role> roles = new HashSet<>();
+                roles.add(role);
                 String pictureUrl = (String) payload.get("picture");
                 String familyName = (String) payload.get("family_name");
                 String givenName = (String) payload.get("given_name");
@@ -47,6 +55,7 @@ public class GoogleService {
                 newUser.setFirstName(givenName);
                 newUser.setLastName(familyName);
                 newUser.setPicture(pictureUrl);
+                newUser.setRoles(roles);
                 userRepository.save(newUser);
                 String jwtToken = jwtService.generateToken(newUser);
                 return new AuthenticationResponse(jwtToken);
