7d01f561a1f9b907eb7a471ed229125e0d6a74d4
vasilhristov1 <>
2024-02-26 15:29:04 +0200

    add forgot password components

---
+++ Frontend/src/Components/ResetPassword.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import { resetPassword } from '../service/ApiService';
+import { useNavigate } from 'react-router-dom';
+
+const ResetPassword = () => {
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [passwordMatchError, setPasswordMatchError] = useState('');
+    const navigate = useNavigate();
+
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const token = urlParams.get('token');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!password) {
+        alert('Please write your new password');
+        return;
+      }
+  
+      if (password.length < 6) {
+        alert('The password has to be more than 6 symbols');
+        return;
+      }
+  
+      if (!confirmPassword) {
+        alert('Please confirm your new password');
+        return;
+      }
+  
+      if (confirmPassword !== password) {
+        alert('Please enter matching passwords');
+        setPasswordMatchError('Passwords do not match!');
+        return;
+      }
+
+      const forgetPasswordRequest = {
+        token: token,
+        password: password
+      }
+  
+      try {
+        await resetPassword(forgetPasswordRequest);
+        alert('Password reset successfully.');
+        navigate('/sign-in');
+      } catch (error) {
+        alert('Failed to reset password: ' + error.response.data);
+      }
+  
+      setPassword('');
+      setConfirmPassword('');
+  };
+
+  return (
+    <div>
+      <h2>Reset Your Password</h2>
+
+      <form onSubmit={handleSubmit} style={{ maxWidth: '350px', margin: '0 auto' }}>
+        <input type="hidden" name="token" value={token} />
+        <div className="border border-secondary rounded p-3">
+          <div>
+            <p>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="form-control"
+                placeholder="Enter your new password"
+                required
+                autoFocus
+              />
+            </p>
+            <p>
+              <input
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                className="form-control"
+                placeholder="Confirm your new password"
+                required
+              />
+            </p>
+            {passwordMatchError && <p className="text-danger">{passwordMatchError}</p>}
+            <p className="text-center">
+              <input type="submit" value="Change Password" className="btn btn-primary" />
+            </p>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ResetPassword;
